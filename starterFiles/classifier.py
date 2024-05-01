9d9b9341b04a742c03548b09e6959603d253f1ef
shawnmont772004 <21b47.shawn@sjec.ac.in>
2024-05-01 16:23:37 +0530

    add placehiolder image which varies on acc

--- starterFiles/classifier.py
+++ starterFiles/classifier.py
@@ -2,6 +2,7 @@ from taipy.gui import Gui
 
 img_path= "logo.png"
 upld_img = " "
+stat_img = "placeholder_image.png"
 
 index = """
 <|text-center|
@@ -9,9 +10,12 @@ index = """
 
 Select an image from your file
 <|{upld_img}|file_selector|>
+
+<|{stat_img}|image|>
 >
 """
 #img_path is the var specified
+#also it can be <|{"logo.png"}|image|> no need of var declaration at the top
 
 app = Gui(page=index)
 
